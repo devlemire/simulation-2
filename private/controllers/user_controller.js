366f8b88037219480a9c4296ee38a6b66af996e2
James Lemire <webdev.jameslemire@gmail.com>
2017-07-05 16:04:00 -0600

    JL master - Login/Register/Logout logic.

--- private/controllers/user_controller.js
+++ private/controllers/user_controller.js
@@ -1,18 +1,35 @@
 module.exports = {
   login: ( req, res, next ) => {
     const db = req.app.get('db');
-    console.log('Login hit');
-    res.status(200).send();
+    const { username, password } = req.body;
+
+    db.find_user([ username, password ]).then( data => {
+      if ( data.length !== 0 ) {
+        const user = data[0];
+
+        req.session.user = {
+          id: user.userid,
+          username: user.username
+        }
+
+        res.status(200).send( req.session.user );
+      } else {
+        res.status(500).send('Invailed username/password.');
+      }
+    }).catch( err => res.status(500).send( err ) );
   },
 
   register: ( req, res, next ) => {
     const db = req.app.get('db');
-    console.log('Register hit');
-    res.status(200).send();
+    const { username, password } = req.body;
+
+    db.register_user([ username, password ]).then( () => {
+      res.status(200).send();
+    }).catch( err => res.status(500).send( err ) );
   },
 
   logout: ( req, res, next ) => {
     req.session.destroy();
-    res.redirect('/login');
+    res.status(200).send('Session destroyed.');
   }
 }
