52a50bccc8b9cd69fc7f95b033947dd4c43f0380
James Lemire <webdev.jameslemire@gmail.com>
2017-07-05 13:15:05 -0600

    JL master - Add auth middleware to backend.

--- private/middlewares/auth.js
+++ private/middlewares/auth.js
@@ -1,1 +1,7 @@
-// If there isn't a user object on req.session, route them to login.
+module.exports = ( req, res, next ) => {
+  if ( req.session.user || req.path === "/api/auth/login" || req.path === "/api/auth/register" ) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+};
