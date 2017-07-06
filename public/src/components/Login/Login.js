a197f9e4a098bae2d46140928805f7f72f3eacef
James Lemire <webdev.jameslemire@gmail.com>
2017-07-06 09:15:25 -0600

    JL master - Add layout to Login

--- public/src/components/Login/Login.js
+++ public/src/components/Login/Login.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
 
+import { Link } from "react-router-dom";
+
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    }
+    
+    this.handleChange = this.handleChange.bind( this );
+  }
+
+  handleChange( prop, val ) {
+    this.setState({ [prop]: val });
+  }
+
   render() {
+    const { username, password } = this.state;
     return (
       <div>
-        Login component here.
+        <span>Username:</span>
+        <input value={ username } onChange={ (e) => this.handleChange('username', e.target.value) } />
+        <br />
+        <span>Password:</span>
+        <input value={ password } type="password" onChange={ (e) => this.handleChange('password', e.target.value) } />
+        <br />
+        <br />
+        <button>Login</button>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
       </div>
     )
   }
