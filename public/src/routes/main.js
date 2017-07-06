1856ec980d34cc6f8c9d9f86e9cfd521efe1ce0b
James Lemire <webdev.jameslemire@gmail.com>
2017-07-06 09:08:50 -0600

    JL master - Add register component

--- public/src/routes/main.js
+++ public/src/routes/main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 
 import App from '../components/App';
 import Login from '../components/Login/Login';
+import Register from '../components/Register/Register';
 import Dashboard from '../components/Dashboard/Dashboard';
 import Wizard from '../components/Wizard/Wizard';
 
@@ -10,6 +11,7 @@ export default (
   <Switch>
     <Route component={ App } path="/" exact />
     <Route component={ Login } path="/login" />
+    <Route component={ Register } path="/register" />
     <Route component={ Dashboard } path="/dashboard" />
     <Route component={ Wizard } path="/wizard" />
   </Switch>
