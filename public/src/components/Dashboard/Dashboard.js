8b5265deedca66aafcc4f90c72aa02513576cc7e
James Lemire <webdev.jameslemire@gmail.com>
2017-07-06 09:00:43 -0600

    JL master - Add dashboard view to routes.

---
+++ public/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Filter from "./Filter/Filter";
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <p> Dashboard here </p>
+        <Filter />
+      </div>
+    )
+  }
+}
